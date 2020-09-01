ef171274781c995798e0c753ee76e6674db5b8cf
NaikMV <60404232+NaikMV@users.noreply.github.com>
2020-09-01 17:51:08 -0400

    Change Name

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -85,7 +85,7 @@ function Header(props) {
                   <h1 className="responsive-headline">Hi, I'm <span
                   class="txt-rotate"
                   data-period="2000"
-                  data-rotate='[ "Manan Naik.", "Coder..!" ]'></span></h1>
+                  data-rotate='[ "Manan", "Coder..!" ]'></span></h1>
                   <h3>I'm a NYC based <span>software engineer</span> currently seeking a full-time software development role in the United States.</h3>
                   <hr />
                   <ul className="social">
