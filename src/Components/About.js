71459df7e4f653dd7d1bf04bb2ae74be7902e0e0
NaikMV <60404232+NaikMV@users.noreply.github.com>
2020-09-01 17:48:30 -0400

    Update Personal Info

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,6 @@ class About extends Component {
          var name = this.props.data.name;
          var profilepic = "images/" + this.props.data.image;
          var bio = this.props.data.bio;
-         var phone = this.props.data.phone;
          var email = this.props.data.email;
       }
 
@@ -26,7 +25,6 @@ class About extends Component {
                         <h2>Contact Details</h2>
                         <p className="address">
                            <span>{name}</span><br />
-                           <span>{phone}</span><br />
                            <span>{email}</span>
                         </p>
                      </div>
